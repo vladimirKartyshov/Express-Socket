07b64ccba558c455680b959ef6c842df172a6eed
vladimirKartyshov <vovan198080@inbox.ru>
2020-06-16 07:58:54 +0300

    Initional projectt

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, ()=>{
+    console.log('Server is works on 3000');
+});
+
+
+
+
+//скрипт stop добавлять в package-json, чтобы убить процесс на порту, если выдают ошибку, что порт уже запущен
+// скрипты при создании с express-generator
+//
+// "scripts": {
+//     "start": "node ./bin/www",
+//         "stop": "taskkill -f -im node.exe"
+
+
+//скрипты c  express-generator и изминенным start и  запуском nodemon
+// "start": "node index.js",
+//     "dev": "nodemon index.js",
+//      "stop": "taskkill -f -im node.exe"
+
+
+// соответственно при изминении start, module.exports = app;
+//   меняем  на
+// app.listen(3000, ()=>{
+//     console.log('Server is works on 3000');
+// });
