8cef554aed637ebced2dd745ba7893fd9476afe2
vladimirKartyshov <vovan198080@inbox.ru>
2020-06-22 22:54:52 +0300

    socket.io

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+let users = [];
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, ()=>{
-    console.log('Server is works on PORT 3000...');
+io.on('connection', (socket) => {
+    users.push(socket);
+    console.log(`Users: ${users.length}`);
 });
 
+server.listen(3000, ()=>{
+    console.log('Server is works on PORT 3000...');
+});
 
 //не забывать в .gitignore добавлять node_modules
 
